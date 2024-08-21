8570f717f7531313c8f3b8cf16054698f13ca98a
Rahul <ra1290hul@gmail.com>
2024-08-21 21:32:28 +0530

    Adding reporting for testcase fail

--- src/test/java/com/automation/steps/HomePageSteps.java
+++ src/test/java/com/automation/steps/HomePageSteps.java
@@ -1,6 +1,7 @@
 package com.automation.steps;
 
 import com.automation.pages.HomePage;
+import com.automation.utils.ReportManager;
 import io.cucumber.java.en.Then;
 import org.testng.Assert;
 
@@ -11,5 +12,6 @@ public class HomePageSteps {
     @Then("verify user is on home page")
     public void verify_user_is_on_home_page() {
         Assert.assertTrue(homePage.isHomePageDisplayed());
+        ReportManager.attackScreenshot();
     }
 }
